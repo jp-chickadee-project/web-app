364a23a3c63128195cb788ec9a74dc3d3e1b1713
David Germain <davidispizza@gmail.com>
2018-02-03 15:37:01 -0500

    clean

--- src/router/index.js
+++ src/router/index.js
@@ -1,16 +1,14 @@
 import Vue from 'vue';
 import Router from 'vue-router';
-import HelloWorld from '@/components/HelloWorld';
+import HomePage from '@/components/HomePage';
+import BirdList from '@/components/birds/List';
 
 Vue.use(Router);
 
 export default new Router({
   mode: 'history',
   routes: [
-    {
-      path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld,
-    },
+    { path: '/', component: HomePage },
+    { path: '/birds', component: BirdList },
   ],
 });
