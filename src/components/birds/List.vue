f3ecc28dbbb0b96f569e45dae58b7a6cf7448830
David Germain <davidispizza@gmail.com>
2018-02-03 16:07:36 -0500

    Pass simple obejct'

--- src/components/birds/List.vue
+++ src/components/birds/List.vue
@@ -1,17 +1,32 @@
 <template>
-  <div class="hello">
+  <div>
+    bird list
+    <ListDetail
+      v-for="bird in birds"
+      :key="bird.rfid"
+      :bird="bird"/>
   </div>
 </template>
 
 <script>
+import ListDetail from './ListDetail';
+
 export default {
-  name: 'HelloWorld',
+  components: {
+    ListDetail,
+  },
+  name: 'List',
   data() {
     return {
+      birds: [
+        { rfid: 'hi' },
+        { rfid: 'hii' },
+        { rfid: 'hiii' },
+        { rfid: 'hiiii' },
+      ],
     };
   },
 };
 </script>
 
-<style scoped>
-</style>
+<style scoped></style>
---
+++ src/components/birds/ListDetail.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>{{bird.rfid}}</div>
+</template>
+
+<script>
+export default {
+  name: 'list-detail',
+  props: {
+    bird: {
+      type: Object,
+      required: true,
+    },
+  },
+  data() {
+    return {};
+  },
+};
+</script>
+
+<style scoped></style>
