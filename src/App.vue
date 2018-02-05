54486b06f1f520cf9b7f86d45e30eaa01730bd31
David Germain <davidispizza@gmail.com>
2018-02-04 23:43:31 -0500

    kinda nice navbar

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,35 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.jpg">
-    <router-view/>
+    <nav class="navbar navbar-toggleable-md navbar-light bg-faded">
+      <button
+        class="navbar-toggler navbar-toggler-right"
+        type="button"
+        data-toggle="collapse"
+        data-target="#navbarNavDropdown"
+        aria-controls="navbarNavDropdown"
+        aria-expanded="false"
+        aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <a class="navbar-brand" href="/">
+        <img src="./assets/logo.jpg"
+          class="d-inline-block align-top"
+          width="30"
+          height="30"
+          alt="">JP Chickadee Project
+      </a>
+      <div class="collapse navbar-collapse" id="navbarNavDropdown">
+        <ul class="navbar-nav">
+          <li class="nav-item">
+            <router-link class="nav-link" to="/">Home</router-link>
+          </li>
+          <li class="nav-item">
+            <router-link class="nav-link" to="/birds">Birds</router-link>
+          </li>
+        </ul>
+      </div>
+    </nav>
+    <router-view></router-view>
   </div>
 </template>
 
