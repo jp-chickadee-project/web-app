bc663256266aff018d504e676dd3874c6f915757
David Germain <davidispizza@gmail.com>
2018-02-06 17:52:47 -0500

    use pug

--- src/components/birds/ListDetail.vue
+++ src/components/birds/ListDetail.vue
@@ -1,21 +1,17 @@
-<template>
-<div class="card">
-  <div class="card-header">
-    <h5 class="mb-0">
-      <button
+<template lang="pug">
+div(class="card")
+  class(class="card-header")
+    h5(class="mb-0")
+      button(
         class="btn btn-link"
         data-toggle="collapse"
         aria-expanded="true"
         :aria-controls="bird.rfid"
-        :data-target="'#' + bird.rfid">{{bird.bandCombo}}</button>
-    </h5>
-    <div
+        :data-target="'#' + bird.rfid") {{bird.bandCombo}}
+    div(
       class="collapse"
-      :id="bird.rfid">
-      <div class="card-body">words</div>
-    </div>
-  </div>
-</div>
+      :id="bird.rfid")
+      div(class="card-body") words
 </template>
 
 <script>
