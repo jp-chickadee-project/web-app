a5320e5573819e58f49aaaf84238e53f088ff170
David Germain <davidispizza@gmail.com>
2018-02-10 17:43:52 -0500

    style and link

--- src/components/HomePage.vue
+++ src/components/HomePage.vue
@@ -4,8 +4,9 @@ div(id="container") This website is under development.
   br
   br
   br
-  p Join bird club!
-  p Meetings are Tuesday at noon in the CS lab.
+  h1 Join bird club!
+  h4 Meetings are Tuesday at noon in the CS lab (JXJ 2214).
+  a(href="https://sites.google.com/view/alecrlindsay/research/jp-chickadee-project") check out the biology website for more information
 </template>
 
 <script>
