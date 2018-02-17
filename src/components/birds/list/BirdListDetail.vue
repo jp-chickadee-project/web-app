f90fecb331920eae06d2e3e61c26faf7c7a8af11
David Germain <davidispizza@gmail.com>
2018-02-16 23:21:46 -0500

    link to profile

--- src/components/birds/list/BirdListDetail.vue
+++ src/components/birds/list/BirdListDetail.vue
@@ -8,6 +8,7 @@ div(class="card")
         aria-expanded="true"
         :aria-controls="bird.rfid"
         :data-target="'#' + bird.rfid") {{bird.bandCombo}}
+      a(:href="'birds/' + bird.rfid") view profile
     div(
       class="collapse"
       :id="bird.rfid")
