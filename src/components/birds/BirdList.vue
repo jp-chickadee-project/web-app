7209705de660922f7c7a40e856f55297a6bf2e77
David Germain <davidispizza@gmail.com>
2018-02-16 22:59:30 -0500

    fix

---
+++ src/components/birds/BirdList.vue
@@ -0,0 +1,34 @@
+<template lang="pug">
+div
+  bird-list-detail(
+    v-for="bird in birds"
+    :key="bird.rfid"
+    :bird="bird")
+</template>
+
+<script>
+import Api from '@/api';
+import BirdListDetail from './BirdListDetail';
+
+export default {
+  components: {
+    BirdListDetail,
+  },
+  name: 'bird-list',
+  data() {
+    return {
+      birds: [],
+    };
+  },
+  created() {
+    Api.get('/birds')
+      .then((response) => {
+        console.log(response.data);
+        this.birds = response.data;
+      })
+      .catch(() => {});
+  },
+};
+</script>
+
+<style scoped></style>
