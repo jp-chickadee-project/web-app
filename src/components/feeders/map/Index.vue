211138ef871279fc9fd21ee4a4f8acd6a6bb5540
David Germain <davidispizza@gmail.com>
2018-03-04 22:58:13 -0500

    set bounds

--- src/components/feeders/map/Index.vue
+++ src/components/feeders/map/Index.vue
@@ -20,11 +20,19 @@ export default {
   },
   mounted() {
     const ZOOM = 15.5;
+    const bounds = new L.LatLngBounds(
+      new L.LatLng(46.558923, -87.440042),
+      new L.LatLng(46.547893, -87.418094)
+    );
     const map = L.map('idmap', {
+      center: bounds.getCenter(),
       minZoom: ZOOM,
-      maxZoom: ZOOM
+      maxZoom: ZOOM,
+      maxBounds: bounds,
+			maxBoundsViscosity: 0.75,
     });
     map.removeControl(map.zoomControl);
+    map.dragging.disable();
 
     L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
       attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors',
