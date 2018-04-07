78083d0c914cc2645d13351638267ee5ac6d3877
David Germain <davidispizza@gmail.com>
2018-04-07 00:45:26 -0400

    setup container

---
+++ src/components/molecules/AssociationsList.vue
@@ -0,0 +1,36 @@
+<template>
+<v-flex>
+  <v-card class='list'>
+    hi
+  </v-card>
+</v-flex>
+</template>
+
+<script>
+import * as _ from 'lodash';
+
+import Analytics from '@/Analytics';
+
+import { getColorForFeeder } from '@/defaults/colors';
+
+export default {
+  components: {},
+  name: 'AssociationsList',
+
+  data() {
+    return {};
+  },
+
+  watch: {},
+
+  mounted() {
+
+  },
+};
+</script>
+
+<style scoped>
+.list {
+  min-height: 40px;
+}
+</style>
