3ce4285c3cab675beaa1a11dc95e4f61bde1810a
David Germain <davidispizza@gmail.com>
2018-04-22 09:53:00 -0400

    remove pug

--- src/components/organisms/BirdProfile.vue
+++ src/components/organisms/BirdProfile.vue
@@ -1,25 +1,24 @@
-<template lang='pug'>
-div
-  <v-container grid-list-md>
-    <v-layout>
-      <v-flex md2>
-        <v-avatar :size='168' :tile='true'>
-          <img src="./../../assets/bird.jpg">
-        </v-avatar>
-      </v-flex>
-      <v-flex>
-        h1 {{bird.bandCombo}}
-        div {{bird.speciesDisplay}}
-        div suspected sex: {{bird.suspectedSex}}
-        div capture date: {{new Date(bird.captureTimestamp * 1000).toGMTString()}}
-      </v-flex>
-    </v-layout>
-    <v-layout row wrap>
-      <VisitsByFeeder :rfid='rfid' :duration='duration' class='xs12 | sm6 | md4'/>
-      <MovementMap :rfid='rfid' :duration='duration' class='xs12 | sm6 | md4'/>
-      <AssociationsList :rfid='rfid' class='xs12 | sm6 | md4'/>
-    </v-layout>
-  </v-container>
+<template>
+<v-container grid-list-md>
+  <v-layout>
+    <v-flex md2>
+      <v-avatar :size='168' :tile='true'>
+        <img src="./../../assets/bird.jpg">
+      </v-avatar>
+    </v-flex>
+    <v-flex>
+      <h1>{{bird.bandCombo}}</h1>
+      <div>{{bird.speciesDisplay}}</div>
+      <div>suspected sex: {{bird.suspectedSex}}</div>
+      <div>capture date: {{new Date(bird.captureTimestamp * 1000).toGMTString()}}</div>
+    </v-flex>
+  </v-layout>
+  <v-layout row wrap>
+    <VisitsByFeeder :rfid='rfid' :duration='duration' class='xs12 | sm6 | md4'/>
+    <MovementMap :rfid='rfid' :duration='duration' class='xs12 | sm6 | md4'/>
+    <AssociationsList :rfid='rfid' class='xs12 | sm6 | md4'/>
+  </v-layout>
+</v-container>
 </template>
 
 <script>
